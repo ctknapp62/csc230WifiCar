32720db75e485820571f94fa95207bd68c9f36c1
Craig <ctknapp62@gmail.com>
2016-10-03 22:51:33 -0700

    Node Server
    Code to run Node Server

---
+++ Node/server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+// var statusLED = new statusLED();
+// statusLED.init();
+
+var five = require('johnny-five');
+var board = new five.Board();
+var servo;
+
+board.on("ready", function() {
+        servo = new five.Servo(9);
+        servo.min();
+        servo.max();
+        servo.center();
+
+        this.repl.inject({servo: servo});
+});
+
+// setup directory for static files
+app.use(express.static('static'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/steer/:direction', function (req,res) {
+  var cw = (req.params.direction == 'Right' ? -1 : 1);
+  servo.step(20 * cw);
+  console.log(req.params);
+  res.send('');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
