e2e8fd6b9f0cbba2db022640d803940f48e88337
Craig <ctknapp62@gmail.com>
2016-10-10 16:01:24 -0700

    Comments added to server.js and proto.html proto.html seperated html and javascript server.js fixed dead zone and added in steering tuning logic

--- Node/server.js
+++ Node/server.js
@@ -1,56 +1,163 @@
+//require pkg express and johnny five in node_moduels
 var express = require('express');
+
+//**********************************
+//****** Requires Arduino **********
+var jfive = require('johnny-five');
+
+//instantiate a new instance of express
 var app = express();
 
-// var statusLED = new statusLED();
-// statusLED.init();
-
-var five = require('johnny-five');
-var board = new five.Board();
-var steeringServo;
+//**********************************
+//****** Requires Arduino **********
+//instantiate a new instance of johnny-five
+var board = new jfive.Board();
+var turnServo;
 var driveServo;
 
+//**********************************
+//****** Requires Arduino **********
 board.on("ready", function() {
-        steeringServo = new five.Servo(9);
-        steeringServo.min();
-        steeringServo.max();
-        steeringServo.center();
+    //Creating the Steering Servo
+    turnServo = new jfive.Servo(9);
+    turnServo.min();
+    turnServo.max();
+    turnServo.center();
 
-        driveServo = new five.Servo(10);
-        driveServo.center();
+    //Creating the drive motor (is actually a servo)
+    driveServo = new jfive.Servo({
+        pin: 10,
+        startAt: 94
 
-        this.repl.inject({steeringServo: steeringServo});
+        //Possibly implement continuous instead of step?
+        // , type: "continuous"
+    });
+
+    //Center both drive and steering servos
+    driveServo.to(90);
+    turnServo.to(90);
+
+
+    //Establish repl commands
+    this.repl.inject({
+        turnServo: turnServo
+    });
+    this.repl.inject({
+        driveServo: driveServo
+    });
+
 });
 
-// setup directory for static files
+//Set up which port express will be listening on and printing a console log
+app.listen(2300, function() {
+    console.log('Port 2300 in use. Used for CSC 230 RC Car');
+});
+
+//**********************************
+//****Requires static Directory*****
+//Link express to directory containing our html files
 app.use(express.static('static'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//handeling URL backslash
+app.get('/', function(req, res) {
+    res.send('Welcome to CSC 230 RC Car!');
 });
 
-app.get('/steer/:direction', function (req,res) {
-  var cw = (req.params.direction == 'Right' ? 1 : -1);
-  steeringServo.step(20 * cw);
-  console.log(req.params);
-  res.send('');
+
+//***********************************
+//********Turning Logic**************
+//maxTurn is used to keep steering position less then max turn radius
+var maxTurn = 0;
+
+//turnRadius is used keep a var that holds the current turn position in degrees
+var turnRadius = 90;
+
+//Setting up the Steering HTTP Req from HTML
+app.get('/steer/:dir', function(req, res) {
+    var steeringDriver = req.params.dir;
+    //Checking direction right and if we are able to still move the servo
+    if (steeringDriver == 'Right' && maxTurn <= 3) {
+        turnRadius = turnRadius + 10;
+        turnServo.to(turnRadius);
+        maxTurn = maxTurn + 1;
+    }
+
+    //Checking direction left and if we are able to still move the servo
+    else if (steeringDriver == 'Left' && maxTurn >= -3) {
+        turnRadius = turnRadius - 10;
+        turnServo.to(turnRadius);
+        maxTurn = maxTurn - 1;
+    }
+
+    //Assume if recived a req and is not left or right it is center command
+    else {
+        turnRadius = 90;
+        turnServo.to(turnRadius);
+        maxTurn = 0;
+    }
+
+    //Lines for debugging in repl
+    console.log(req.params);
+    console.log("Turn Radius " + turnRadius);
+    console.log("MaxTurn " + maxTurn);
+    res.send('');
 });
 
-app.get('/drive/:fr', function (req,res)  {
-  var direction = req.params.fr;
-  if (direction == 'Forward') {
-    driveServo.step(1);
-  }
-  else if (direction == 'Reverse') {
-    driveServo.to(87);
-  }
-  else if(direction == 'Stop') {
-    driveServo.center(); 
-  }
-  console.log(req.params);
-  res.send('');
 
+//*****************************************
+//*************Drive Logic*****************
+//driveSpeed is a var that holds the cars current speed
+// 94-88 driveSpeed is dead zone where car wont move
+var driveSpeed = 94;
+
+//Setting upt the Drive HTTP Req from HTML
+app.get('/drive/:dir', function(req, res) {
+    //Setting the direction of drive from http req
+    var driveDirection = req.params.dir;
+    if (driveDirection == 'Forward') {
+        driveSpeed = driveSpeed + 1;
+        //Check to see if driveSpeed is in dead zone while trying to go forward
+        if (driveSpeed == 95 || driveSpeed == 91) {
+            driveSpeed = 102;
+        }
+        driveServo.to(driveSpeed);
+    }
+    //Check to see if driveSpeed is in dead zone while trying to go reverse
+    else if (driveDirection == 'Reverse') {
+        driveSpeed = driveSpeed - 1;
+        if (driveSpeed == 93 || driveSpeed == 98) {
+            driveSpeed = 87;
+        }
+        driveServo.to(driveSpeed);
+
+    }
+    //Assume if not forward or reverse recived a 'Stop' command
+    else {
+        driveServo.to(94);
+        driveSpeed = 94;
+
+    }
+    //Lines for debugging in repl
+    console.log(req.params);
+    console.log(driveSpeed);
+    res.send('');
 });
 
-app.listen(2300, function () {
-  console.log('Example app listening on port 2300!');
+
+//***************************************************
+//*************Calibration Logic*********************
+app.get('/cal/:dir', function(req, res) {
+    var driveDirection = req.params.dir;
+    if (driveDirection == 'Max') {
+        turnServo.to(180);
+    } else if (driveDirection == 'Min') {
+        turnServo.to(0);
+
+    } else {
+        turnServo.to(90);
+
+    }
+    console.log(req.params);
+    console.log(driveSpeed);
+    res.send('');
 });
